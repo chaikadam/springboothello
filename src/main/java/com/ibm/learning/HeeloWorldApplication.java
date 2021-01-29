e5cdb18b4fc84cc25479d556f71c5eadba2d8ca0
chaikadam <76508479+chaikadam@users.noreply.github.com>
2021-01-29 14:48:06 +0530

    FirstSpringProject

---
+++ src/main/java/com/ibm/learning/HeeloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeeloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeeloWorldApplication.class, args);
+	}
+
+}
